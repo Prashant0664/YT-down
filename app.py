def6d219625db244398d69f94b3d795d938537bc
Prashant <98750053+Prashant0664@users.noreply.github.com>
2024-10-08 01:14:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 from flask import Flask, request
 from pytube import YouTube
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+# Allow CORS from localhost:5000
+CORS(app, origins=["https://www.youtube.com"])
 
 def download(link):
     youtubeObject = YouTube(link)
     yt = youtubeObject.streams.get_highest_resolution()
     try:
         yt.download('Downloads')
-    except:
-        print("An error has occurred")
-    print(link + " Download is completed successfully")
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+    print(f"{link} Download is completed successfully")
 
 @app.get('/summary')
-# @cross_origin()
 def summary_api():
-    print(1111)
     url = request.args.get('url', '')
     download(url)
     return "summary", 200
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
