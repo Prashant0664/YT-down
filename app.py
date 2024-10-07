fc6a90da4b5fd126850cfc8f0477ae8f78c7b695
Prashant <98750053+Prashant0664@users.noreply.github.com>
2024-10-08 00:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 from flask import Flask, request
-from youtube_transcript_api import YouTubeTranscriptApi
-from transformers import pipeline
 from pytube import YouTube
 app = Flask(__name__)
 
+def download(link):
+    youtubeObject = YouTube(link)
+    yt = youtubeObject.streams.get_highest_resolution()
+    try:
+        yt.download('Downloads')
+    except:
+        print("An error has occurred")
+    print(link + " Download is completed successfully")
+
 @app.get('/summary')
+# @cross_origin()
 def summary_api():
     print(1111)
     url = request.args.get('url', '')
-    video_id = url.split('=')[1]
-    # summary = get_summary(get_transcript(video_id))
+    download(url)
     return "summary", 200
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
-def get_transcript(video_id):
-    print(video_id)
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-    transcript = ' '.join([d['text'] for d in transcript_list])
-    return transcript
-
-def get_summary(transcript):
-    print(1111345425)
-    summariser = pipeline('summarization')
-    summary = ''
-    for i in range(0, (len(transcript)//1000)+1):
-        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
-        summary = summary + summary_text + ' '
-    return summary
-
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    try:
-        youtubeObject.download()
-    except:
-        print("An error has occurred")
-    print("Download is completed successfully")
-
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
